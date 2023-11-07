979a859c35565f2bb64f9bcf9fe0cecbfecfeeb8
mg143pavankumar <pavansharma.mg143@gmail.com>
2023-11-07 00:36:49 -0500

    added functionality to search pages

--- app/(main)/_components/Navigation.tsx
+++ app/(main)/_components/Navigation.tsx
@@ -26,8 +26,10 @@ import {
   PopoverTrigger,
 } from "@radix-ui/react-popover";
 import TrashBox from "./TrashBox";
+import { useSearch } from "@/hooks/use-search";
 
 const Navigation = () => {
+  const search = useSearch();
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
   const create = useMutation(api.documents.create);
@@ -149,7 +151,7 @@ const Navigation = () => {
         </div>
         <div>
           <UserItem />
-          <Item label="Search" icon={Search} isSearch onClick={() => {}} />
+          <Item label="Search" icon={Search} isSearch onClick={search.onOpen} />
           <Item label="Settings" icon={Settings} onClick={() => {}} />
           <Item onClick={handleCreate} label="New page" icon={PlusCircle} />
         </div>
---
+++ components/search-command.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { useQuery } from "convex/react";
+import { useRouter } from "next/navigation";
+import { useUser } from "@clerk/clerk-react";
+import { api } from "@/convex/_generated/api";
+import { useSearch } from "@/hooks/use-search";
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "./ui/command";
+import { File } from "lucide-react";
+
+export const SearchCommand = () => {
+  const { user } = useUser();
+  const router = useRouter();
+  const documents = useQuery(api.documents.getSearch);
+
+  const [isMounted, setIsMounted] = useState(false);
+
+  const toogle = useSearch((store) => store.toggle);
+  const isOpen = useSearch((store) => store.isOpen);
+  const onClose = useSearch((store) => store.onClose);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key == "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        toogle();
+      }
+    };
+
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, [toogle]);
+
+  const onSelect = (id: string) => {
+    router.push(`/documents/${id}`);
+    onClose();
+  };
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <CommandDialog open={isOpen} onOpenChange={onClose}>
+      <CommandInput placeholder={`Search ${user?.fullName}'s Jotion...`} />
+      <CommandList>
+        <CommandEmpty>No results found.</CommandEmpty>
+        <CommandGroup heading="Documents">
+          {documents?.map((doc) => {
+            return (
+              <CommandItem
+                key={doc._id}
+                value={`${doc._id}-${doc.title}`}
+                title={doc.title}
+                onSelect={onSelect}
+              >
+                {doc.icon ? (
+                  <p className="mr-2 text-[18px]">{doc.icon}</p>
+                ) : (
+                  <File className="mr-2 h-4 w-4" />
+                )}
+                <span>{doc.title}</span>
+              </CommandItem>
+            );
+          })}
+        </CommandGroup>
+      </CommandList>
+    </CommandDialog>
+  );
+};
